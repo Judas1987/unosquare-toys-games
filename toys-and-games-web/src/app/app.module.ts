36f589f602bb3ad032076c77dc2fea36df3e862e
Judas Echeverria <judas1987@gmail.com>
2020-11-16 00:25:21 -0700

    A small improvement to the UX, I implemented an error interceptor for HTTP unhandled exception and a quick message is shown

--- toys-and-games-web/src/app/app.module.ts
+++ toys-and-games-web/src/app/app.module.ts
@@ -7,7 +7,7 @@ import {ProductEditComponent} from './products/product-edit/product-edit.compone
 import {ProductsComponent} from './products/products.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CurrencyPipe} from '@angular/common';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {DeleteConfirmationDialogComponent} from './delete-confirmation-dialog/delete-confirmation-dialog.component';
 import {MatDialogModule} from '@angular/material/dialog';
@@ -15,6 +15,10 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+import {ErrorDisplayComponent} from './error-display/error-display.component';
+import {HttpErrorInterceptor} from './Services/HttpErrorInterceptor';
 
 @NgModule({
   declarations: [
@@ -22,7 +26,8 @@ import {MatButtonModule} from '@angular/material/button';
     ProductListComponent,
     ProductEditComponent,
     ProductsComponent,
-    DeleteConfirmationDialogComponent
+    DeleteConfirmationDialogComponent,
+    ErrorDisplayComponent
   ],
   imports: [
     BrowserModule,
@@ -35,10 +40,22 @@ import {MatButtonModule} from '@angular/material/button';
     MatToolbarModule,
     MatIconModule,
     MatDividerModule,
-    MatButtonModule
+    MatButtonModule,
+    MatProgressBarModule
   ],
   providers: [
-    CurrencyPipe
+    CurrencyPipe,
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: {
+        appearance: 'fill'
+      }
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
