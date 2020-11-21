dc143c2a6c01b23a00f174d281dbee9e11006fb6
Judas Echeverria <judas@clidigitalmedia.com>
2020-11-20 21:52:10 -0700

    Missing javac documentation to the functions in the service class

--- toys-and-games-web/src/app/Services/products.service.ts
+++ toys-and-games-web/src/app/Services/products.service.ts
@@ -1,31 +1,43 @@
 import {Injectable} from '@angular/core';
 import {Product} from '../models/Product';
-import {Guid} from 'guid-typescript';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from '../../environments/environment';
-import {Observable, Subscription} from 'rxjs';
+import {Observable} from 'rxjs';
 import {BaseResponse} from '../models/BaseResponse';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ProductsService {
-    private products: Product[] = [];
-    private addedProducts: Product[] = [];
 
+    /**
+     * Class constructor
+     * @param httpClient Represents the HTTP client instance.
+     */
     constructor(private httpClient: HttpClient) {
     }
 
+    /**
+     * This function gets a product by its identifier.
+     * @param productId Represents the product identifier.
+     */
     getProductById(productId: string): Observable<BaseResponse<Product>> {
         const apiResource = `${environment.productsApiUrl}/products/${productId}`;
         return this.httpClient.get<BaseResponse<Product>>(apiResource);
     }
 
+    /**
+     * This function gets all the existing products from the API.
+     */
     getProducts(): Observable<BaseResponse<Product>> {
         const apiResource = `${environment.productsApiUrl}/products`;
         return this.httpClient.get<BaseResponse<Product>>(apiResource);
     }
 
+    /**
+     * This function sends a POST request to the products API in order to create a new entry in the DB.
+     * @param newProduct Represents the product to be added.
+     */
     addProduct(newProduct: Product): Observable<BaseResponse<Product>> {
         const apiResource = `${environment.productsApiUrl}/products`;
 
@@ -36,6 +48,11 @@ export class ProductsService {
         });
     }
 
+    /**
+     * This function sends a PUT request to the product API in order to update an existing product.
+     * @param productId Represents the product id to be used to update a specific product.
+     * @param existingProduct Represents the instance of the existing product.
+     */
     updateProduct(productId: string, existingProduct: Product): Observable<BaseResponse<Product>> {
         const apiResource = `${environment.productsApiUrl}/products/${productId}`;
 
@@ -46,6 +63,10 @@ export class ProductsService {
         });
     }
 
+    /**
+     * This function sends a DELETE request to a given resource in the product API.
+     * @param productId Represents the product identifier.
+     */
     deleteProduct(productId): Observable<any> {
         const apiResource = `${environment.productsApiUrl}/products/${productId}`;
 
